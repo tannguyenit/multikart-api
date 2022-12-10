cb978f6d219e2403865fe9698ad8c8283c01ea7c
Tân Nguyễn <tannguyenit95@gmail.com>
2022-12-10 14:36:55 +0700

    [Bugfix] Get thumbnail image

--- src/utils/app.js
+++ src/utils/app.js
@@ -1,11 +1,14 @@
 const getImageThumbnail = (images) => {
   const baseUrl = process.env.APP_URL;
-
-  if (Array.isArray(images)) {
-    return `${baseUrl}/${images[0]}`;
+  if (!images) {
+    return '';
   }
 
-  return `${baseUrl}/${images}`;
+  if (Array.isArray(images)) {
+    return `${baseUrl}${images[0]}`;
+  }
+
+  return `${baseUrl}${images}`;
 };
 
 module.exports = {
